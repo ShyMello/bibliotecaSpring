e91f4711dcaeb3bbed4381be91e99eeacf166c27
20151074010239 <20151074010239@MC266448.ifrn.local>
2018-10-30 13:50:21 -0300

    Configuração do spring.

---
+++ bibliotecaspring/src/bibliotecaspring/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package bibliotecaspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String homeTeste() {
+		System.out.println("Chamou o método da home! #EleNão");
+		return "home.jsp";
+	}
+
+}
