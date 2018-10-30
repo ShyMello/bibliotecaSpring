e7c43e1b2c54cec6f55e61cc5660eb22f997681b
20151074010239 <20151074010239@MC266448.ifrn.local>
2018-10-30 15:03:33 -0300

    Criação do Controller do Aluno. Criação do DAOS.

---
+++ bibliotecaspring/src/bibliotecaspring/controllers/AlunoController.java
@@ -0,0 +1,5 @@
+package bibliotecaspring.controllers;
+
+public class AlunoController {
+
+}
---
+++ bibliotecaspring/src/bibliotecaspring/daos/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package bibliotecaspring.daos;
+
+public class ConnectionFactory {
+	private static Connection con = null;
+	private static final String banco = "spring";
+	private static final String user = "root";
+	private static final String password = "lab02";
+}
